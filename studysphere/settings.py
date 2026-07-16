176916b2982f4eeb3e95f060ba81a0a6aea61b76
Takashi-exe <ibrahimtobi145@gmail.com>
2026-07-16 16:57:34 +0100

    Regenerate requirements.txt from working local environment

--- studysphere/settings.py
+++ studysphere/settings.py
@@ -161,3 +161,24 @@ CACHES = {
         }
     }
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
