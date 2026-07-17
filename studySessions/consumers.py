ee700247b262ba91be41d132269c180ea001d849
Takashi-exe <ibrahimtobi145@gmail.com>
2026-07-17 19:38:04 +0100

    ...

--- studySessions/consumers.py
+++ studySessions/consumers.py
@@ -1,5 +1,6 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
 from .models import StudySession
 
 class TimerConsumer(AsyncWebsocketConsumer):
